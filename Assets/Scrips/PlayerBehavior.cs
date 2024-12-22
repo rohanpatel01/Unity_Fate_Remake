11b023d53b00a16b8fe1dd51087b824c0a18f4f7
rohanpatel01 <rohanppatel01@gmail.com>
2024-12-22 03:43:35 -0600

    Only move player on non enemy attack and stop move if attack enemy

--- Assets/Scrips/PlayerBehavior.cs
+++ Assets/Scrips/PlayerBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 public class PlayerBehavior : MonoBehaviour
@@ -5,10 +6,27 @@ public class PlayerBehavior : MonoBehaviour
     public Camera cam;
     // public NavMeshAgent player;
     public GameObject targetDest;
+    private NavMeshAgent playerNavMeshAgent;
 
-    
+
+    void Start()
+    {
+        playerNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
     // Update is called once per frame
     void Update()
+    {
+        playerMovement();
+        attackEnemy();
+    }
+
+    void LateUpdate()
+    {
+        cameraMovement();
+    }
+
+    void playerMovement()
     {
         if (Input.GetMouseButtonDown(0)) 
         {
@@ -17,23 +35,38 @@ public class PlayerBehavior : MonoBehaviour
 
             if(Physics.Raycast(ray, out hitPoint))
             {
-                targetDest.transform.position = hitPoint.point;
-                NavMeshAgent player = GetComponent<NavMeshAgent>();
-                player.SetDestination(hitPoint.point);
+                if (hitPoint.collider.tag != "Enemy")
+                {
+                    targetDest.transform.position = hitPoint.point;
+                    playerNavMeshAgent.SetDestination(hitPoint.point);
+
+                } else 
+                {
+                    playerNavMeshAgent.SetDestination(transform.position);
+                }
+                
+            }
+        }
+    }
+    void attackEnemy()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayInfo;
+
+        if(Physics.Raycast(ray, out rayInfo) && Input.GetMouseButtonDown(0))
+        {
+            if (rayInfo.collider.tag == "Enemy")
+            {
+                Debug.Log("Enemy");
             }
         }
     }
 
-    void LateUpdate()
-    {
-        CameraMovement();
-    }
-
-    void CameraMovement()
+    void cameraMovement()
     {
         Vector3 camera_offset = new Vector3(0.355670035f,21.1875229f,-20.8973522f);
-
         cam.transform.position = transform.position + camera_offset;
-
     }
+    
+    
 }
